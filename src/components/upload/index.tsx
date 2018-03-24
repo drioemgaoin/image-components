7268250f918b3c43821a62f7d2ba961016a62fd6
romain diiegoni <romain.diegoni@perfectchannel.com>
2018-03-24 15:38:16 +0000

    Link file explorer when clicking on "Select a file from your computer"

--- src/components/upload/index.tsx
+++ src/components/upload/index.tsx
@@ -2,20 +2,71 @@ import * as React from 'react';
 
 export interface UploadProps {}
 
-export class Upload extends React.Component<UploadProps, undefined> {
-    private onClickBound = this.onClick.bind(this);
+export interface UploadState {
+    processing: boolean;
+    files?: FileList;
+}
+
+export class Upload extends React.Component<UploadProps, UploadState> {
+    private submitBound = this.submit.bind(this);
+    private openFileExplorerBound = this.openFileExplorer.bind(this);
+    private selectFileBound = this.selectFile.bind(this);
+    private inputElement: HTMLInputElement;
+
+    constructor(props: UploadProps) {
+        super(props);
+
+        this.state = {
+            processing: false
+        };
+    }
 
     render() {
         return (
             <div className="Upload">
-                <label className="Upload__drag">Drag a file here</label>
-                <label className="Upload__or">or if you prefer...</label>
-                <button className="Upload__button" onClick={this.onClickBound}>
-                    Select a file from your computer
-                </button>
+                <form className="Upload__form" onSubmit={this.submitBound} encType="multipart/form-data">
+                    <input
+                        ref={input => (this.inputElement = input)}
+                        type="file"
+                        onChange={this.selectFileBound}
+                        style={{ display: 'none' }}
+                    />
+
+                    <label className="Upload__form__drag">Drag a file here</label>
+
+                    <label className="Upload__form__separator">or if you prefer...</label>
+
+                    <button className="Upload__form__select" type="submit" onClick={this.openFileExplorerBound}>
+                        Select a file from your computer
+                    </button>
+
+                    {this.state.files && this.renderFileSelected()}
+                </form>
             </div>
         );
     }
 
-    private onClick() {}
+    private submit(e: HTMLFormElement) {
+        e.preventDefault();
+
+        this.setState({ processing: true, files: null });
+    }
+
+    private openFileExplorer(e: HTMLButtonElement) {
+        this.inputElement.click();
+    }
+
+    private selectFile(e: HTMLInputElement) {
+        this.setState({ files: e.target.files });
+    }
+
+    private renderFileSelected() {
+        return (
+            <div className="Upload__form__result">
+                {Array.from(this.state.files).map((file: File, index: number) => (
+                    <label key={index}>{file.name}</label>
+                ))}
+            </div>
+        );
+    }
 }
